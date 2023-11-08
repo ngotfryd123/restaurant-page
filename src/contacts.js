f012b84869b0ed5d8a6d240c86708069d5df7f88
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-11-08 08:58:01 -0800

    Added menu and contacts content

---
+++ src/contacts.js
@@ -0,0 +1,20 @@
+const people = [{
+    name: "Nick",
+    position: "Owner/manager",
+    phone: "111-111-1111",
+    email:"fakeemail@fakeemail.com"
+},
+
+{
+    name: "Rick",
+    position: "Chef",
+    phone: "111-121-1111",
+    email:"fakeemail@fakeemail.com"
+},
+
+{
+    name: "Nicky",
+    position: "Waiter",
+    phone: "111-131-1111",
+    email:"fakeemail@fakeemail.com"
+}]
---
+++ src/menu.js
@@ -0,0 +1,83 @@
+const drinks = [{
+    name: "Black Coffee",
+    description: "Quick n hot and hits the caffeine spot.",
+    price: "$3"
+},
+
+{
+    name: "Hot Mocha",
+    description: "Espresso, warm milk and a frothy top and a sweet chocolatey twist",
+    price: "$5"
+},
+
+{
+    name: "Latte",
+    description: "Espresso, steamed milk and a final, thin layer of frothed milk on top.",
+    price: "$5"
+},
+
+{
+    name: "Americano",
+    description: "Espresso and water",
+    price: "$4"
+},
+
+{
+    name: "Cappuccino",
+    description: "Perfect balance of espresso, steamed milk and foam.",
+    price: "$5"
+},
+
+{
+    name: "Hot Tea",
+    description: "Choice of green or black tea, with honey and lemon.",
+    price: "$3"
+},
+
+{
+    name: "Hot Chocolate",
+    description: "Shaved and melted chocolate over heated milk.",
+    price: "$3"
+}];
+
+ const dishes = [{
+    name: "Egg, Ham, and Cheese Croissant",
+    description: "Meaty, cheesy perfection in a fluffy croissant.",
+    price: "$10"
+},
+
+{
+    name: "French Toast and Fruit",
+    description: "Warm, buttery, powdered, bread with maple syrup, and your choice of fruit",
+    price: "$10"
+},
+
+{
+    name: "Avocado Toast",
+    description: "Avocado topped with cherry tomatoes, parsley, and a balsamic glaze on toasted brioche.",
+    price: "$10"
+},
+
+{
+    name: "Sweet Crepe",
+    description: "Very thin pancake with nutella and berries.",
+    price: "$10"
+},
+
+{
+    name: "BLT Avocado Croissant",
+    description: "Bacon, lettuce, tomato, avocado, and mayo.",
+    price: "$15"
+},
+
+{
+    name: "Salmon Croisandwich",
+    description: "Smoked salmon, red onions, and house dressing made with dill, capers, and cream cheese",
+    price: "$13"
+},
+
+{
+    name: "Almond Croissant",
+    description: "Toasted almonds and filled with a sweet almond filling or frangipane swirled throughout the dough.",
+    price: "$13"
+}];
