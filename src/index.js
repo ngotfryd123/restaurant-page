45918db82bbc0fccbef73d5728750653e9b71e83
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-11-14 08:09:52 -0800

    added selecting marker and pointer to tabs

--- src/index.js
+++ src/index.js
@@ -5,25 +5,37 @@ import { contactsFunc } from './contacts';
 const clearScreen = () => {
     const content = document.getElementById("content");
     content.innerHTML="";
-    console.log("why");
 };
 
 const home = document.getElementById("home");
 const menu = document.getElementById("menu");
 const contact = document.getElementById("contact");
 
+home.style.cursor="pointer";
+menu.style.cursor="pointer";
+contact.style.cursor="pointer";
+
 home.addEventListener('click',() => {    
     clearScreen();
+    menu.style.backgroundColor = "";
+    contact.style.backgroundColor = "";
+    home.style.backgroundColor = "chocolate";
     functionOne();    
 });
 
 menu.addEventListener('click',() => {    
     clearScreen();
+    home.style.backgroundColor = "";
+    contact.style.backgroundColor = "";
+    menu.style.backgroundColor = "chocolate";
     menuFunc();    
 });
 
 contact.addEventListener('click',() => {    
     clearScreen();
+    home.style.backgroundColor = "";
+    menu.style.backgroundColor = "";
+    contact.style.backgroundColor = "chocolate";
     contactsFunc();    
 });
 
